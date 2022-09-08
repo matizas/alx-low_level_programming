9e7658028f93a39fd3bdcec22f924d19c952f107
matizas <matizasam@gmail.com>
2022-09-08 21:05:21 +0000

    fixed code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,6 +20,5 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lg));
 	printf("Size of a float: %ld bytes(s)\n", sizeof(f));
-
 	return (0);
 }
