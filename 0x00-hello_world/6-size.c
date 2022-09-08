271ca350b01adf61a0cd4787a9babcff3abcb152
matizas <matizasam@gmail.com>
2022-09-08 21:53:49 +0000

    start again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,18 +6,14 @@
  * Description: prints the size of various data types
  * Return: 0 always when successful
 */
+
 int main(void)
 {
 	char a;
 	int b;
 	long c;
 	long long d;
-	float f;
+	float e;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
