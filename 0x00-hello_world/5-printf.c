72d6d3d7da362ace42b32d912085911d998308f7
matizas <matizasam@gmail.com>
2022-09-08 17:41:21 +0000

    with proper grammar, but the outcome is a piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Description: program to display message on the sceen
+ * Return: 0 always when successful
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
