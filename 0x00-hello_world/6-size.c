97724417f272866986a4f511dc6c790118fba30a
matizas <matizasam@gmail.com>
2022-09-08 21:17:04 +0000

    no more errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,22 +3,21 @@
 /**
  * main - program entry point
  *
- * Description: program that prints the size of various types on the computer
+ * Description: prints the size of various data types
  * Return: 0 always when successful
 */
 
 int main(void)
 {
-	char c;
-	int i;
-	long l;
-	long long lg;
+	char a;
+	int b;
+	long c;
+	long long d;
 	float f;
-
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(l));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lg));
+	printf("Size of a char: %ld byte(s)\n", sizeof(a));
+	printf("Size of an int: %ld byte(s)\n", sizeof(b));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
 	printf("Size of a float: %ld bytes(s)\n", sizeof(f));
 	return (0);
 }
