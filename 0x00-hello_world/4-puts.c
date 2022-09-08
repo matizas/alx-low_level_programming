3980fef6b9a9e04b6b4904306f7e822f049c2321
matizas <matizasam@gmail.com>
2022-09-08 17:31:02 +0000

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,9 @@
  * main - program entry point
  *
  * Description: program to display message on the sceen
- * 
  * Return: 0 always when successful
 */
-
+i
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
