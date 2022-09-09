befaf28e2c996f5e0d7d97d360361ca91672c749
matizas <matizasam@gmail.com>
2022-09-09 21:49:06 +0000

    prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the entry point of a program
+ *
+ * Description: print whether the number is positive or negative.
+ * Return: 0 if successful
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		putchar(n+'0');
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
