9fcae5bb88d75fe5db5927c072201efc7b2837a7
matizas <matizasam@gmail.com>
2022-09-09 00:00:38 +0000

    return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Description: This program is used to print a message
+ * The program is to print a errors
+ * Return: 0 will be returned
+ * test - is a function to print a message
+ * Return: 1 test returns: to main
+ */
+
+int test(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
+
+int main(void)
+{
+	test();
+
+	return (0);
+}
