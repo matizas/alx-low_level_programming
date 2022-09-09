ccf8112538bc21f099ea2ac308332df0c551df39
matizas <matizasam@gmail.com>
2022-09-09 22:24:45 +0000

    prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the entry point of a program
+ *
+ * Description: print whether the number is positive or negative.
+ * Return: 0 if successful
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		putchar(n + '0');
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('$');
+
+	return (0);
+}
