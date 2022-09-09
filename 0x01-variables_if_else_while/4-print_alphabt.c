61d3071ee316da056c04b60a4a32cefd097c411e
matizas <matizasam@gmail.com>
2022-09-09 21:10:10 +0000

    prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the entry point of a program
+ *
+ * Description: print whether the number is positive or negative.
+ * Return: 0 if successful
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	char c;
+
+	n = 97;
+	while (n < 123)
+	{
+		c = n;
+		if ((c != 'q') || (c != 'e'))
+			putchar(c);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
