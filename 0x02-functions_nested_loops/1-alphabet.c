5937cdbd3c3dfe1ff63d7ed9cf97eb0b68d27f9e
matizas <matizasam@gmail.com>
2022-09-13 20:07:08 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+nclude "main.h"
+
+/**
+ * *main - print alphaberts
+ * *Return: 0 if successful
+ * */
+
+int main(void)
+{
+	int acsii_value = 97;
+	char l;
+
+	while (acsii_value <= 122)
+	{
+		l = acsii_value
+			_putchar(l);
+		acsii_value++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
