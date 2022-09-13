a70062447b5538f7383556b7253d0bfc481ca760
matizas <matizasam@gmail.com>
2022-09-13 19:55:08 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+
+/**
+ * main - print _putchar
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
