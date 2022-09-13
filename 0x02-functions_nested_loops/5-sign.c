b80592034ed8756205896e4d42a01421612f5c36
matizas <matizasam@gmail.com>
2022-09-13 21:52:31 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,20 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		  return (1);
-		   _putchar('+');
+		return (1);
+		_putchar('+');
+		_putchar(1 + '0');
 	}
 	else if (n == 0)
 	{
-		  return (0);
-		   _putchar('0');
+	       	return (0);
+		_putchar('0');
+		_putchar(0 + '0');
 	}
 	else
 	{
-		  return (-1);
-		   _putchar('-');
+		return (-1);
+		_putchar('-');
+		_putchar(1 + '0');
 	}
 }
