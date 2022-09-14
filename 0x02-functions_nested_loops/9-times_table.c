3fd47ccb2801f54f20c98e052005c97100475377
matizas <matizasam@gmail.com>
2022-09-14 18:49:36 +0000

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+nclude "main.h"
+
+/**
+*void times_table - this function prints 9 times table
+*@n: the function has a parameter n of type integer
+*Return: 0 if successful
+*/
+
+void times_table(void)
+{
+	int w, f, r, c, p;
+
+	r = 0;
+	while(r < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			p = r * c;
+			if (p < 10)
+			{
+				_putchar(p + '0');
+			}
+			else
+			{
+				w = p / 10;
+				f = p % 10;
+				_putchar(w + '0');
+				_putchar(f + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+			c++;
+		}
+		_putchar('\n');
+		r++;
+	}
+}
+_putchar(l + '0');
+return (l);
+}
