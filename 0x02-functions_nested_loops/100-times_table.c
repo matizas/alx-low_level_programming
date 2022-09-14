d9dd137c00a26d3d0c77544bd21d54978a44e17e
matizas <matizasam@gmail.com>
2022-09-14 20:20:00 +0000

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+*print_times_table - this function prints n times table
+*@n - the function recieve data from the calling program
+*Return: 0 if successful
+*/
+
+void print_times_table(int n)
+{
+	int w, f, r, c, p;
+
+	r = 0;
+	while (r < n)
+	{
+		_putchar('0');
+		c = 1;
+		while (c < n)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			p = r * c;
+
+			if (p < 10)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				w = p / 10;
+				_putchar(w + '0');
+			}
+			f = p % 10;
+			_putchar(f + '0');
+			c++;
+		}
+		_putchar('\n');
+		r++;
+	}
+}
+
