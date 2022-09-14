c286ff29ab8360fcbe803cb52bdde507e6cc981f
matizas <matizasam@gmail.com>
2022-09-14 20:54:04 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,48 +9,51 @@
 void print_times_table(int n)
 {
 	int w, f, r, c, p;
-
-	r = 0;
-	while (r <= n)
+	
+	if (n > 0 && n < 16)
 	{
-		_putchar('0');
-		c = 1;
-		while (c <= n)
+		r = 0;
+		while (r <= n)
 		{
-			_putchar(',');
-			_putchar(' ');
-
-			p = r * c;
-
-			if (p < 10)
+			_putchar('0');
+			
+			c = 1;
+			while (c <= n)
 			{
+				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(p + '0');
+				
+				p = r * c;
+				
+				if (p < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(p + '0');
+				}
+				else if (p < 100)
+				{
+					_putchar(' ');
+					w = p / 10;
+					_putchar(w + '0');
+					f = p % 10;
+					_putchar(f + '0');
+				}
+				else
+				{
+					w = p / 100;
+					_putchar(w + '0');
+					f = p % 100;
+					w = f / 10;
+					_putchar(w + '0');
+					f = f % 10;
+					_putchar(f + '0');
+				}
+				c++;
 			}
-			else if (p < 100)
-			{
-				_putchar(' ');
-				w = p / 10;
-				_putchar(w + '0');
-				f = p % 10;
-				_putchar(f + '0');
-			}
-			else
-			{
-				w = p / 100;
-				_putchar(w + '0');
-
-				f = p % 100;
-				w = f / 10;
-				_putchar(w + '0');
-				f = f % 10;
-				_putchar(f + '0');
-			}
-			c++;
+			_putchar('\n');
+			r++;
 		}
-		_putchar('\n');
-		r++;
 	}
 }
 
