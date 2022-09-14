d265565157b7deb8123ceffdb2d5d862527d48ea
matizas <matizasam@gmail.com>
2022-09-14 23:26:01 +0000

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -23,7 +23,7 @@ int main(void)
 
 void print_sum_fib(long n)
 {
-	long p = 1, c = 2;
+	long p = 1, c = 1;
 	long tmp, total, countTerm;
 
 	total = c;
@@ -37,7 +37,7 @@ void print_sum_fib(long n)
 		c = c + p;
 		p = tmp;
 
-		if (countTerm % 2 != 0)
+		if (countTerm % 2 == 0)
 		{
 			total = total + c;
 		}
