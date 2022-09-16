ba49a36e781465cefcf244763f1881ac93fc0a4c
matizas <matizasam@gmail.com>
2022-09-16 11:35:11 +0000

    Readme

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+void print_sum(int);
+void print_fib(int);
+void print_sum_fib(long);
+void print_fib_terms(int);
+int largest_number(int a, int b, int c);
+void positive_or_negative(int);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif
