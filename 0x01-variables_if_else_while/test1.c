9fed2da1cfc32b40c4da36f813609b895bf83f32
matizas <matizasam@gmail.com>
2022-09-16 06:00:19 +0000

    C-Debugging

---
+++ 0x01-variables_if_else_while/test1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This is the preferred style for multi-line
+ * comments in C source code.
+ * Please use it consistently.
+ *
+ * Description:  A column of asterisks on the left side,
+ * Return: 0 with beginning and ending almost-blank lines.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
