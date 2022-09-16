c9d8115e445229956ca80c29f43146ba89675513
matizas <matizasam@gmail.com>
2022-09-16 11:54:11 +0000

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -22,4 +22,5 @@ int largest_number(int a, int b, int c);
 void positive_or_negative(int);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
+int _isupper(int c);
 #endif
