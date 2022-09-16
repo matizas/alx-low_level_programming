e8710d9a0e8b0cb219fc0987042a028cf5dec165
matizas <matizasam@gmail.com>
2022-09-16 21:52:17 +0000

    for (i = 1; i <= size; i++)

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+void print_fizz_buzz(void);
 
 /**
  * main - to call a method fizz_buzz
@@ -9,24 +10,38 @@
 
 int main(void)
 {
-	int i, j;
-
-
-	for (i = 1; i <= 100; i++)
-	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%s ", "FizzBuzz");
-
-		else if (i % 5 == 0)
-			printf("%s ", "Buzz");
-
-		else if (i % 3 == 0)
-			printf("%s ", "Fizz");
-
-		else
-			printf("%d ", i);
-	}
-	printf("\n");
+	print_fizz_buzz();
 
 	return (0);
+}
+
+/**
+ * print_fizz_buzz - print a square
+ *
+ * Return: does not return a value
+ */
+
+void print_fizz_buzz(void)
+{
+	int i, j;
+	char a[] = "Fizz";
+	char b[] = "Buzz";
+	char ab[] = "FizzBuzz";
+
+	printf("%d", 1);
+	for (i = 2; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf(" %s", ab);
+
+		else if (i % 5 == 0)
+			printf(" %s", b);
+
+		else if (i % 3 == 0)
+			printf(" %s", a);
+
+		else
+			printf(" %d", i);
+	}
+	printf("\n");
 }
--- 0x04-more_functions_nested_loops/99-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/99-fizz_buzz.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-void print_fizz_buzz(void);
 
 /**
  * main - to call a method fizz_buzz
@@ -10,38 +9,24 @@ void print_fizz_buzz(void);
 
 int main(void)
 {
-	print_fizz_buzz();
-
-	return (0);
-}
-
-/**
- * print_fizz_buzz - print a square
- *
- * Return: does not return a value
- */
-
-void print_fizz_buzz(void)
-{
 	int i, j;
-	char a[] = "Fizz";
-	char b[] = "Buzz";
-	char ab[] = "FizzBuzz";
 
 
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%s ", ab);
+			printf("%s ", "FizzBuzz");
 
 		else if (i % 5 == 0)
-			printf("%s ", b);
+			printf("%s ", "Buzz");
 
 		else if (i % 3 == 0)
-			printf("%s ", a);
+			printf("%s ", "Fizz");
 
 		else
 			printf("%d ", i);
 	}
 	printf("\n");
+
+	return (0);
 }
