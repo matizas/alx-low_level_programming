39b35f3d4cdb18926856a3cf065926be527fae9b
matizas <matizasam@gmail.com>
2022-09-17 00:09:40 +0000

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * prime_number - print an integer value
+ * @n: integer parameter
+ */
+
+void print_number(int n)
+{
+	int w;
+
+	if (n < 0)
+	{
+		w = -n;
+		_putchar('_');
+	}
+	else
+	{
+		w = n;
+	}
+
+	if (w / 10)
+	{
+		w = w / 10;
+		print_number(w);
+	}
+
+	w = w % 10;
+	_putchar(w + '0');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -33,4 +33,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_fizz_buzz(void);
 void print_triangle(int size);
+void print_number(int n);
+void prime_factors(long int);
 #endif
