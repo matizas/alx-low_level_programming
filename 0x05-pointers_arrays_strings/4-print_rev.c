4eb7ba28768aaf298765d71f158b65d59f519e72
matizas <matizasam@gmail.com>
2022-09-19 19:50:24 +0000

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+int _strlen(char *s);
 /**
  * print_rev - reversing a string
  *
@@ -18,4 +19,31 @@ void print_rev(char *s)
 		i--;
 	}
 	_putchar('\n');
+}
+
+/**
+ * _strlen - to count number of characters
+ *
+ * @s: reference parameter to recieve a string
+ * Return: an integer, the Length or the number of characters.
+ */
+
+int _strlen(char *s)
+{
+	int i, flag;
+
+	i = 0;
+	flag = 0;
+
+	while (!flag)
+	{
+		if (s[i] != '\0')
+		{
+			i++;
+		}
+		else
+			flag = 1;
+	}
+
+	return (i);
 }
