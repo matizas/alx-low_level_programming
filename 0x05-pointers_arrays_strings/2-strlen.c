a597cc76f61c34a465737ffe8fd30c55af7aa9c7
matizas <matizasam@gmail.com>
2022-09-19 18:48:20 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - to count number of characters
+ *
+ * @s: reference parameter to recieve a string
+ * Return: an integer, the Length or the number of characters.
+ */
+
+int _strlen(char *s)
+{
+	int iLength;
+
+	iLength = strlen(s);
+
+	return (iLength);
+}
