3980c63202cb814594c297d4d772f0d1db98703b
matizas <matizasam@gmail.com>
2022-09-19 19:42:39 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - reversing a string
+ *
+ * @s: reference parameter to store a string
+ * Return: does not return a value
+ */
+
+void print_rev(char *s)
+{
+	int i, flag;
+
+	i = _strlen(s) - 1;
+
+	while (i > 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
