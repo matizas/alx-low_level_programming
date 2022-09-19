ce72a8a3e6b9ffc2a63f06fb4ab9df2756216496
matizas <matizasam@gmail.com>
2022-09-19 19:56:31 +0000

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,49 @@
+#include "main.h"
+int _strlen(char *s);
+/**
+ * rev_string - reversing a string
+ *
+ * @s: reference parameter to store a string
+ * Return: does not return a value
+ */
+
+void rev_string(char *s)
+{
+	int i;
+
+	i = _strlen(s) - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - to count number of characters
+ *
+ * @s: reference parameter to recieve a string
+ * Return: an integer, the Length or the number of characters.
+ */
+
+int _strlen(char *s)
+{
+	int i, flag;
+
+	i = 0;
+	flag = 0;
+
+	while (!flag)
+	{
+		if (s[i] != '\0')
+		{
+			i++;
+		}
+		else
+			flag = 1;
+	}
+
+	return (i);
+}
