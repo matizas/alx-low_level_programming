863fde4e7f40ae175dfedf61077d585737160334
matizas <matizasam@gmail.com>
2022-09-19 21:55:34 +0000

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,7 +16,7 @@ void print_array(int *a, int n)
 	for (i = 0; i <= n - 1; i++)
 	{
 		if (i == n - 1)
-			printf("%d", *(a + i));
+			printf("%d\n", *(a + i));
 		else
 			printf("%d, ", *(a + i));
 	}
