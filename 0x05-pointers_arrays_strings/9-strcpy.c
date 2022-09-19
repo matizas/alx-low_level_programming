b805378adb7685b9fe4677a9c8f07b73740acc68
matizas <matizasam@gmail.com>
2022-09-19 23:08:54 +0000

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copy a string from one address to the other
+ * @dest: first ref parameter
+ * @src: second ref parameter
+ * Return: a poniter is successful
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, flag;
+
+	i = 0;
+	flag = 0;
+	while (!flag)
+	{
+		if (src[i] != '\0')
+		{
+			*(dest + i) = *(src + 1);
+			i++;
+		}
+		else
+			flag = 1;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest);
+}
