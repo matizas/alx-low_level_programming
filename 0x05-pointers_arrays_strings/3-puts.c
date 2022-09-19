0e2a08388677b642b126e0b08653952b31d7fcc9
matizas <matizasam@gmail.com>
2022-09-19 19:04:11 +0000

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - print to stdio a string
+ *
+ * @str: reference parameter to recieve a string.
+ * Return: does not return a value
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
