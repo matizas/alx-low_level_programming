06bfa5e37ae01b6207f1ebfddef82317ada07766
matizas <matizasam@gmail.com>
2022-09-20 02:01:15 +0000

    void puts_half(char *str)

--- 0x05-pointers_arrays_strings/88-print_array.c
+++ 0x05-pointers_arrays_strings/88-print_array.c
@@ -13,11 +13,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i <= n - 1; i++)
 	{
-		printf("%d", *(a + i));
-		if (i < n - 1)
-			printf(", ");
+		if (i == n - 1)
+			printf("%d\n", a[i]);
+		else
+			printf("%d, ", a[i]);
 	}
-	printf("\n");
 }
