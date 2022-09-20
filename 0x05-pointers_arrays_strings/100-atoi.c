b282efd498d57ed3872fe4737066c6eca7a36e5a
matizas <matizasam@gmail.com>
2022-09-20 02:36:58 +0000

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - convert from string to integer;
+ *
+ * @s: ref parameter to recieve a string
+ * Return: an integer value is returned
+ */
+
+int _atoi(char *s)
+{
+	int neg, i, num;
+
+	neg = 1;
+	i = 0;
+	num = 0;
+
+	while (!(s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			neg = neg * -1;
+		}
+
+		i++;
+	}
+
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
+	{
+		num = (num * 10) + (s[i] - '0');
+		i++;
+	}
+
+	num = num * neg;
+	return (num);
+}
+
