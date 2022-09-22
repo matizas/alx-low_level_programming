3c88a94d560612ea96dbfa40787a8728c5f0ba60
matizas <matizasam@gmail.com>
2022-09-22 21:12:29 +0000

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *leet - to encrypt a string
+ *
+ * @s: ref parameter
+ * Return: the address
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+	char letter[] = "aAeEoOtTlL";
+	char digits[] = "4433007711";
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (letter[j] != '\0')
+		{
+			if (s[i] == letter[j])
+			{
+				s[i] = digits[j];
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+}
