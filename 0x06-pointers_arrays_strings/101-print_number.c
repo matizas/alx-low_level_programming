76409edbe9bea38a6c8c6ba1a12b99859a5dd52b
matizas <matizasam@gmail.com>
2022-09-22 07:03:09 +0000

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,13 +8,13 @@
 
 void print_number(int n)
 {
-	if (num < 0)
+	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		n = -n;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
-	_putchar((num % 10) + '0');
+	if ((n / 10) > 0)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 }
