39521e7a7151a9acef44188f998c05ae8b89b3d8
matizas <matizasam@gmail.com>
2022-09-22 17:53:17 +0000

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * *cap_string - capitalize words
+ *
+ * @s: ref parameter
+ * Return: address
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		switch (s[i])
+		{
+			case 9:
+			case 10:
+			case 32:
+			case 33:
+			case 34:
+			case 40:
+			case 41:
+			case 44:
+			case 46:
+			case 59:
+			case 63:
+			case 123:
+			case 125:
+				{
+					if (s[i + 1] >= 97 && s[i + 1] <= 122)
+					{
+						s[i + 1] = s[i + 1] - 32;
+					}
+					break;
+				}
+		}
+
+		i++;
+	}
+
+	return (s);
+}
