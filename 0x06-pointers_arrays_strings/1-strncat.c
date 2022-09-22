84bc07be8920f451e004a2d3e8be30063f4eebc7
matizas <matizasam@gmail.com>
2022-09-22 07:36:50 +0000

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+int _strlen(char *s);
+
+/**
+ * *_strncat - to merge two strings
+ *
+ * @dest: first ref parameter
+ * @src: second ref parameter
+ * @n: value parameter
+ * Return: an address
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int l_dest, i;
+
+	l_dest = _strlen(dest);
+
+	i = 0;
+	while (i < n)
+	{
+		dest[l_dest] = src[i];
+		l_dest++;
+		i++;
+	}
+
+	return (dest);
+}
+
+/**
+ * _strlen - count the number of characters in a string
+ *
+ * @s: ref parameter
+ * Return: number of characters
+ */
+
+int _strlen(char *s)
+{
+	int strLength;
+
+	strLength = 0;
+
+	while (s[strLength] != '\0')
+	{
+		strLength++;
+	}
+
+	return (strLength);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@
 int _putchar(char);
 char *_strcat(char *dest, char *src);
 void print_number(int n);
+char *_strncat(char *dest, char *src, int n);
 #endif
