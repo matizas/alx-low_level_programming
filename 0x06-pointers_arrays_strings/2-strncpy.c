aac0691200e4fcc710092a4c7e1c9a1b0fe1177c
matizas <matizasam@gmail.com>
2022-09-22 09:11:32 +0000

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,57 @@
+#include "main.h"
+int _strlen(char *s);
+
+/**
+* *_strncpy - to merge two strings
+*
+* @dest: first ref parameter
+* @src: second ref parameter
+* @n: value parameter
+* Return: an address
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int l_src, i;
+
+	l_src = _strlen(src);
+
+	if (n > l_src)
+	{
+		n = l_src;
+	}
+
+	if (n > sizeof(dest))
+	{
+		n = sizeof(dest);
+	}
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+}
+
+/**
+* _strlen - count the number of characters in a string
+*
+* @s: ref parameter
+* Return: number of characters
+*/
+
+int _strlen(char *s)
+{
+	int strLength;
+
+	strLength = 0;
+
+	while (s[strLength] != '\0')
+	{
+		strLength++;
+	}
+
+	return (strLength);
+}
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,4 +5,5 @@ int _putchar(char);
 char *_strcat(char *dest, char *src);
 void print_number(int n);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif
