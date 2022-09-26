e3be070455a714e0f4d22fd72b48829c413788b8
matizas <matizasam@gmail.com>
2022-09-26 23:18:24 +0000

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,27 +8,26 @@
  * Return: does not return a value
  */
 
-
 void print_diagsums(int *a, int size)
 {
-	int r, c, sum_right_diag, sum_left_diag;
+	int r, c, sum_right, sum_left;
 
 	r = 0;
-	sum_right_diag = 0;
+	sum_right = 0;
 
 	while (r <= (size * size))
 	{
-		sum_right_diag = sum_right_diag + a[r];
+		sum_right = sum_right + a[r];
 		r = r + size + 1;
 	}
 
 	c = 0;
-	sum_left_diag = 0;
+	sum_left = 0;
 	while (c < (size * size - 1))
 	{
-		sum_left_diag = sum_left_diag + a[c];
+		sum_left = sum_left + a[c];
 		c = c + size - 1;
 	}
 
-	printf("%d, %d\n", sum_right_diag, sum_left_diag);
+	printf("%d, %d\n", sum_right, sum_left);
 }
