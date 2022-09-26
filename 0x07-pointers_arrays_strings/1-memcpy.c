fdaf9d07d6aa0d753c76d51c1f331d55bc037e1a
matizas <matizasam@gmail.com>
2022-09-26 11:51:26 +0000

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * *_memcpy - copies memory area from one source to the other
+ *
+ * @dest: first ref parameter
+ * @src: second ref parameter
+ * @n: value parameter, number of bytes
+ * Return: address the destination address
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
