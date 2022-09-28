b21d2aebe1acec19faad74efaf2d6c50a201b4c6
matizas <matizasam@gmail.com>
2022-09-28 21:52:06 +0000

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,8 +17,10 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*(s2 + 1) == '*')
 			return (wildcmp(s1, s2 + 1));
-		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+		else if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 			return (1);
+		else
+			return (0);
 	}
 	else
 		return (0);
