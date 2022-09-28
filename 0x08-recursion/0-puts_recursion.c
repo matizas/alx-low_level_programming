5de52f4c5d4df4cb181ba79f8f18ab5334a9541d
matizas <matizasam@gmail.com>
2022-09-28 12:39:29 +0000

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - recursively print characters in a string
+ *
+ * @s: ref parameter to store the address of the string
+ * Return: does not return anything
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+
+	}
+}
