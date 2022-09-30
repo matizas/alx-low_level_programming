f733e28f2f6d9ef6d0cdfc1b5a5deae8dbe0be73
matizas <matizasam@gmail.com>
2022-09-30 21:37:38 +0000

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,53 +2,34 @@
 #include <stdlib.h>
 
 /**
- * main - program to determine number of arguments
- * @argc: value parameter to store number of arguments
- * @argv: ref parameter to store a string of arguments
- * Return: does not return anything
+ * main - add positive numbers
+ * @argc: number of arguments
+ * @argv: array of string arguments
+ * Return: Always 0 if successful else 1
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int flag = 0, i = 1, j, sum = 0;
-	char c = '0';
+	int i, j, sum = 0;
 
-	while (i < argc)
-	{
-		j = 0;
-		while (argv[i][j] != '\0' && !flag)
-		{
-			if (argv[i][j] < 48 || argv[i][j] > 57)
-			{
-				c = '1';
-				flag = 1;
-			}
-			j++;
-		}
-		i++;
-	}
-
-	if (c == '0')
+	if (argc < 1)
+		printf("0\n");
+	else
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) > 0)
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				sum += atoi(argv[i]);
+				if (argv[i][j] < 48 || argv[i][j] > 57)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+			if (atoi(argv[i]) > 0)
+				sum += atoi(argv[i]);
 		}
-		printf("%d\n", sum);
-	}
-
-	else if (argc < 1)
-	{
-		printf("0\n");
-	}
-
-	else
-	{
-		printf("ERROR\n");
-		return (1);
+		printf("%i\n", sum);
 	}
 	return (0);
 }
