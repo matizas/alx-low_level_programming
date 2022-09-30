462c19ac1a2f53b8a0ae47cb752340fc516fa421
matizas <matizasam@gmail.com>
2022-09-30 08:28:00 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - program to determine number of arguments
+ * @argc: value parameter to store number of arguments
+ * @argv: ref parameter to store a string of arguments
+ * Return: does not return anything
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argv[0][i] != '\0')
+	{
+		_putchar(argv[0][i]);
+		i++;
+	}
+	return (0);
+}
