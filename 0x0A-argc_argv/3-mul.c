29e61a9d11a285d22c18279289c1598e49c0529b
matizas <matizasam@gmail.com>
2022-09-30 19:59:44 +0000

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program to determine number of arguments
+ * @argc: value parameter to store number of arguments
+ * @argv: ref parameter to store a string of arguments
+ * Return: does not return anything
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, p;
+	
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		
+		p = a * b;
+		printf("%d\n", p);
+		return (0);
+	}
+	else
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+}
