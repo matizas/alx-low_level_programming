42582bb67f7c8b891090127c91933d9a3f2bc037
matizas <matizasam@gmail.com>
2022-10-05 05:40:36 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 	int **iarray;
 	int i, n;
 
-	if (width <= 0 || height <= 0)
+	if (width < 0 || height < 0)
 		return (NULL);
 
 	iarray = malloc(sizeof(int) * height);
