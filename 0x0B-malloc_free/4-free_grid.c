ec05c5749014546824a7cdd03e00d70980163dae
matizas <matizasam@gmail.com>
2022-10-05 04:11:00 +0000

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - to free alooc_grid 2D array
+ * @grid: pointer to 2D array
+ * @height: rows of a 2D array
+ * Return: does not return a value
+ */
+
+void free_grid(int **grid, int height)
+{
+	while (height > 0)
+	{
+		free(grid[height]);
+		height--;
+	}
+
+	free(grid);
+}
