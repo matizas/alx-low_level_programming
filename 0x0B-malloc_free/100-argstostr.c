7463595141ac9d5935d363774c2aec036c55f209
matizas <matizasam@gmail.com>
2022-10-05 11:56:22 +0000

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *argstostr - to concatenate arguments
+ * @ac: number of arguments
+ * @av: an array of string
+ * Return: pointer to the string or NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int Words, i, k = 0, lenStr = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	Words = 0;
+	while (Words < ac)
+	{
+		i = 0;
+		while (av[Words][i] != '\0')
+		{
+			lenStr++;
+			i++;
+		}
+		lenStr++;
+		Words++;
+	}
+	lenStr += ac;
+
+	str = malloc(sizeof(char) * lenStr + 1);
+	if (str == NULL)
+		return (NULL);
+
+	for (Words = 0; Words < ac; Words++)
+	{
+		for (i = 0; av[Words][i]; i++)
+		{
+			str[k] = av[Words][i];
+			k++;
+		}
+		if (str[k] == '\0')
+		{
+			str[k++] = '\n';
+		}
+	}
+	return (str);
+}
