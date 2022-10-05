def9d1b161fedd2b8dd8ccc54087363f04bf7235
matizas <matizasam@gmail.com>
2022-10-05 09:11:28 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arrNum = malloc(sizeof(int) * (height + 1));
+	arrNum = malloc(sizeof(int *) * height);
 	if (arrNum == NULL)
 		return (NULL);
 
