85036b7241e0ef6076ec2f34c88032963889f091
matizas <matizasam@gmail.com>
2022-10-06 18:56:27 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,7 +17,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 
-	if (s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
 	dest = malloc((n + i) * sizeof(char) + 1);
