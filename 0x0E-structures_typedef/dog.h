bfa470ca224c715f06d69eb24e520371228c007f
matizas <matizasam@gmail.com>
2022-10-10 20:52:49 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - to define the feutures of the object dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+ 
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
