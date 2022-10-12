5445dfa7b6f59b3948df8f82aaca892b150736be
matizas <matizasam@gmail.com>
2022-10-12 22:16:06 +0000

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - to print elements of an array
+ * @array: get the address of the array
+ * @size: to store the size of an array
+ * @action: pointer to function to print the elements of the array
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
