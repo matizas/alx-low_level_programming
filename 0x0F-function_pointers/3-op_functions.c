5aaead2609f416f899a47f2e1d65fac6976b7372
matizas <matizasam@gmail.com>
2022-10-13 00:25:10 +0000

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - addition operator
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of two integers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction operator
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of two integers
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication operator
+ * @a: first integer
+ * @b: second integer
+ * Return: product of two integers
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division operator
+ * @a: first integer
+ * @b: second integer
+ * Return: quotient of two integers
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_add - modulus operator
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of two integers
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
