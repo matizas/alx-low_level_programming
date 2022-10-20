17aa87feab282ecbe9dd756531e355b1f39e9774
matizas <matizasam@gmail.com>
2022-10-20 21:58:09 +0000

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the elements in the linked list
+ * @h: the node of the linked list
+ * Return: void
+ */
+
+size_t print_list(const list_t *h)
+{
+	char *s = "[0] (nil)";
+	int i = 0, count = 0;
+
+	if (h->str == NULL)
+	{
+		while (s[i] != '\0')
+		{
+			_putchar(s[i]);
+			i++;
+		}
+	}
+
+	while (h->next != NULL)
+	{
+		i = 0;
+		while (h->str[i] != '\0')
+		{
+			_putchar(h->str[i]);
+			i++;
+		}
+		_putchar('\n');
+		count++;
+	}
+	return (count);
+}
