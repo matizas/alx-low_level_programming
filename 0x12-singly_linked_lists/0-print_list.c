897ac06e83493822dec19ea322ec2b8d69ac293f
matizas <matizasam@gmail.com>
2022-10-20 22:28:09 +0000

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,15 +15,16 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("%s\n", "[0], (nil)");
 
 		else
 
-			printf("[%d] %s\n", h->len, h->str);
+			printf("%s\n", h->str);
 
 		h = h->next;
 
 		count++;
 	}
+	printf("\n");
 	return (ount);
 }
