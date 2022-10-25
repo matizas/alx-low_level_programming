da088d5a8cd395be813eb9935e8eb4212c399a8c
matizas <matizasam@gmail.com>
2022-10-25 21:21:09 +0000

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -27,18 +27,12 @@ typedef struct listint_s
  * Description: singly linked list node structure
  */
 
-typedef struct list_s
+typedef struct listp_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
