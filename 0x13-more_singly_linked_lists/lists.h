1e61f37490b686e05a3fa38fdf8c3099e9b4221a
matizas <matizasam@gmail.com>
2022-10-25 19:50:12 +0000

    1-listint_len.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -27,8 +27,6 @@ typedef struct listint_s
  * Description: singly linked list node structure
  */
 
-
-
 typedef struct list_s
 {
 	char *str;
@@ -42,4 +40,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 #endif
