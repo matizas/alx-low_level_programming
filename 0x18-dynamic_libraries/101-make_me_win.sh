6ec758e6167f4dd3dc908dd42898665f1bbfe2ae
matizas <matizasam@gmail.com>
2022-12-12 21:36:59 +0000

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/holbertonschool/raw/master/0x18.c/win.so
+wget -O /tmp/win.so https://github.com/holbertonschool/0x18.c/win.so
 export LD_PRELOAD=/tmp/win.so
